2118179ef89a627428cfb2dd1b9a4b44b0215e90
Cuu <cuu9991@gmail.com>
2025-01-04 18:24:11 +0700

    Check json exist

--- disable_useless_services.py
+++ disable_useless_services.py
@@ -3,7 +3,7 @@ import json
 import os
 
 
-def get_list_useless_services(json_path="useless_services.json"):
+def get_list_useless_services(json_path: str) -> list:
     with open(json_path, "r", encoding="utf-8") as file:
         data = json.load(file)
     return data["useless_services"]
@@ -13,8 +13,12 @@ if __name__ == "__main__":
     run_as_admin()
 
     try:
-        list_useless_services = get_list_useless_services()
-        for service_name in list_useless_services:
-            disable_service(service_name)
+        json_path = "useless_services.json"
+        if os.path.exists(json_path):
+            list_useless_services = get_list_useless_services(json_path)
+            for service_name in list_useless_services:
+                disable_service(service_name)
+        else:
+            print(f"Khong tim thay file {json_path}")
     finally:
         os.system("pause")
--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
-pyinstaller --clean --optimize=2 --noconfirm --onefile D:\MythProject\Windows10Tweak\disable_useless_services.py
+# Py to Exe
+pyinstaller --clean --optimize=2 --noconfirm --onefile --name="DisableServices" disable_useless_services.py
+pyinstaller --clean --optimize=2 --noconfirm --onedir --name="DisableServices1" disable_useless_services.py
+pyinstaller DisableServices.spec
